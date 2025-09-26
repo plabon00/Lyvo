038c85b63137d77e77d05ce54573cd1d91222036
plabon00 <sarkarplabon576@gmail.com>
2025-09-26 15:16:49 +0530

    Initial commit

---
+++ src/main/java/com/apex/lyvo/LyvoApplication.java
@@ -0,0 +1,13 @@
+package com.apex.lyvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyvoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyvoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apex/lyvo/LyvoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.lyvo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyvoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
